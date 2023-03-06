c885a04b742596668ce66469062863ebaac03237
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2023-03-06 16:21:42 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,12 +9,12 @@ sudo apt-get install -y \
     gnupg \
     lsb-release
 #  Add Docker’s official GPG key:
-sudo mkdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 # Use the following command to set up the repository:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null -y
 # Update the apt package index, and install the latest version of Docker Engine, containerd, and Docker Compose, or go to the next step to install a specific version:
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
